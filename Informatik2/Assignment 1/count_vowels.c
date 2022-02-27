1067d685725a02c3f1e8fe8aeb99250efbfcacea
Queentaker <93202854+Queentaker@users.noreply.github.com>
2022-02-27 16:47:02 +0100

    Update count_vowels.c
    possible solution

--- Informatik2/Assignment 1/count_vowels.c
+++ Informatik2/Assignment 1/count_vowels.c
@@ -1,45 +1,28 @@
-#include <string.h>
-#include <stdio.h>
-
-int count_vowels(char A[]) {
-
-    int i, n, count;
-    n = strlen(A);
-    count = 0;
-
+void BSm(char A[1000]){
+    int i,n;
+    char BS[3000]={};
+    char b='b';
+    int h=0;
+    n=strlen(A);
     for(i = 0; i <= n; i++){
-        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' || 
-        A[i] == 'A' || A[i] == 'E' || A[i] == 'I'|| A[i] == 'O' || A[i] ==  'U') {
-            count++;
+        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' ||
+           A[i] == 'A' || A[i] == 'E' || A[i] == 'I'|| A[i] == 'O' || A[i] ==  'U') {
+            BS[h]=A[i];
+            BS[h+1]=b;
+            BS[h+2]=A[i];
+            h=h+3;
         }
+        else{
+            BS[h]=A[i];
+            h+=1;
+        }
+
+        }
+    int r=strlen(BS);
+    int s=0;
+    for (s;s<=r; s++){
+        printf("%c",BS[s]);
     }
 
-    return count;
-
-}
-
-char* BS(char A[]){
-    char sol[50];
-    int i, j;
-    int n = strlen(A);
-
-    for(i = 0; i < n; i++) {
-        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' || 
-        A[i] == 'A' || A[i] == 'E' || A[i] == 'I'|| A[i] == 'O' || A[i] ==  'U') {
-            
-        } else {}
-    }
-
-
-    return sol;
-}
-
-int main() {
-
-    int num;
-    char A[] = "Hello Worldi";
-
-    num = count_vowels(A);
-    printf("The number of vowels in the given string is: %d", num);
 
 }
